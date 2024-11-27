f2b3d77e20971b92844012efa859a807efe609ac
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-11-27 17:35:08 +0100

    Populate Header with components

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import PromoBar from "./PromoBar";
+import Logo from "../../ui/Logo";
+import Nav from "./Nav";
+import MenuCart from "./MenuCart";
+
 export default function Header() {
-  return <header>Header</header>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="border-b border-neutral-200 bg-white">
+      <PromoBar />
+      <div className="container flex items-center justify-between py-4">
+        <Logo />
+        <Nav isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <MenuCart isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      </div>
+    </header>
+  );
 }

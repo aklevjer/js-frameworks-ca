1dabf91a4b1a65ac7370e1206756901f0af5b686
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-11-28 21:29:23 +0100

    Update Home page to display the product list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,19 @@
+import { useProducts } from "../../hooks/useProducts";
+import ProductList from "../../components/product/ProductList";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const { products, isLoading, isError } = useProducts();
+
+  return (
+    <section>
+      <div className="mb-12 space-y-4 text-center">
+        <h1 className="text-4xl font-bold capitalize">Everything you need in one place</h1>
+        <p>Browse our selection and find the perfect products for your needs.</p>
+      </div>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Oops! Failed to load products. Please try again later.</p>}
+      {products && <ProductList products={products} />}
+    </section>
+  );
 }

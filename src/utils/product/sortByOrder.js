0a0336c3ccebb716f8a2ce0098673e11b3fea30b
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-11-28 23:19:13 +0100

    Add utility functions for filtering and sorting products

---
+++ src/utils/product/sortByOrder.js
@@ -0,0 +1,16 @@
+export function sortByOrder(products, sortOrder) {
+  return [...products].sort((a, b) => {
+    switch (sortOrder) {
+      case "title-asc":
+        return a.title.localeCompare(b.title);
+      case "title-desc":
+        return b.title.localeCompare(a.title);
+      case "price-asc":
+        return a.price - b.price;
+      case "price-desc":
+        return b.price - a.price;
+      default:
+        return 0;
+    }
+  });
+}

756d48ec7eb5eb0d955aa23aa47af508bc848bd3
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-11-28 23:22:22 +0100

    Add SortSelect component

---
+++ src/components/ui/SortSelect/index.jsx
@@ -0,0 +1,27 @@
+export default function SortSelect({ onSort, disabled }) {
+  const handleSort = (event) => {
+    const sortOrder = event.target.value;
+    onSort(sortOrder);
+  };
+
+  return (
+    <div className="space-y-2 sm:basis-52">
+      <label htmlFor="sort" className="text-m font-medium">
+        Sort by
+      </label>
+
+      <select
+        id="sort"
+        onChange={handleSort}
+        disabled={disabled}
+        className="w-full rounded-md border-r-8 border-r-transparent bg-white p-2 outline-none ring-1 ring-neutral-500 focus:ring-2 focus:ring-neutral-900 disabled:opacity-60"
+      >
+        <option value="default">Default</option>
+        <option value="title-asc">Name (A-Z)</option>
+        <option value="title-desc">Name (Z-A)</option>
+        <option value="price-desc">Price (High to low)</option>
+        <option value="price-asc">Price (Low to high)</option>
+      </select>
+    </div>
+  );
+}

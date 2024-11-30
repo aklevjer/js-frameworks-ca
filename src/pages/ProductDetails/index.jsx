88c34d76ee74e1e894f0733d4c5cc6fbcdd2fd5a
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-11-30 11:10:10 +0100

    Update product details page to display product info

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -1,3 +1,26 @@
+import { useParams } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+import { useProductById } from "../../hooks/useProductById";
+
+import Button from "../../components/ui/Button";
+import Loader from "../../components/ui/Loader";
+import ErrorMessage from "../../components/ui/ErrorMessage";
+import SingleProduct from "../../components/product/SingleProduct";
+
 export default function ProductDetails() {
-  return <h1>Product details</h1>;
+  const { id } = useParams();
+  const { product, isLoading, isError } = useProductById(id);
+
+  return (
+    <section className="space-y-8">
+      <Button asLink variant="secondary" path="/" className="inline-flex items-center gap-2">
+        <BiArrowBack size={18} />
+        <span>Go back</span>
+      </Button>
+
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage message="Oops! Failed to load product. Please try again later." />}
+      {product && <SingleProduct product={product} />}
+    </section>
+  );
 }

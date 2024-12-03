1bf58c7ca610506f876f6c127f38678c4616a8d8
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-12-03 18:11:47 +0100

    Update contact page with details and form

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,3 +1,14 @@
+import ContactDetails from "../../components/contact/ContactDetails";
+import ContactForm from "../../components/contact/ContactForm";
+
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <section>
+      <h1 className="mb-12 text-center text-3xl font-bold capitalize">Contact us</h1>
+      <div className="grid gap-12 md:grid-cols-2">
+        <ContactDetails />
+        <ContactForm />
+      </div>
+    </section>
+  );
 }

599365ae9fabb974ccf45d67611fd18cad95babf
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-03-01 18:04:15 +0100

    Add JSDocs to API functions

--- src/api/products.js
+++ src/api/products.js
@@ -1,5 +1,11 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+/**
+ * Fetches products from the API.
+ *
+ * @returns {Promise<Object[]>} A promise that resolves to an array of product objects upon successful retrieval.
+ * @throws {Error} If there is an error during the retrieval process.
+ */
 export async function getProducts() {
   const response = await fetch(API_URL);
   const responseData = await response.json();
@@ -12,6 +18,14 @@ export async function getProducts() {
   return responseData.data;
 }
 
+/**
+ * Fetches a product by its ID from the API.
+ *
+ * @param {string} productId - The ID of the product.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing product data upon successful retrieval.
+ * @throws {Error} If there is an error during the retrieval process.
+ */
 export async function getProductById(productId) {
   const response = await fetch(`${API_URL}/${productId}`);
   const responseData = await response.json();
